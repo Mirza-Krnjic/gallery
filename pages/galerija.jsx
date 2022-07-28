1ebbf7742c298262561aca30f8fefdefc0583e37
Krnjic Mirza <krnjicmirza@gmail.com>
2022-07-28 21:08:55 +0200

    add pagination

--- pages/galerija.jsx
+++ pages/galerija.jsx
@@ -1,18 +1,65 @@
 import React from "react"
 import { client } from "../lib/client"
 import { Product } from "../components"
+import { useState } from "react"
+import ReactPaginate from "react-paginate"
+import styles from "./galerija.module.css"
 
 function galerija({ products }) {
+  const [pageNumer, setPageNumber] = useState(0)
+  const usersPerPage = 10
+  const pagesVisited = pageNumer * usersPerPage
+
+  const displayUsers = products
+    .slice(pagesVisited, pagesVisited + usersPerPage)
+    .map((product) => {
+      return <Product key={product._id} product={product} />
+    })
+
+  const pageCount = Math.ceil(products.length / usersPerPage)
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected)
+  }
+
   return (
     <>
       <div className="products-heading">
         <h2>Galerija</h2>
         <p>Razni umjetnici i njihova djela</p>
       </div>
+      <div className={styles["pagination-container"]}>
+        <ReactPaginate
+          previousLabel={"Prethodna"}
+          nextLabel={"Sljedeća"}
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={styles.paginationBtns}
+          previousLinkClassName={"previousBtn"}
+          nextLinkClassName={"nextBtn"}
+          disabledClassName={"paginationDisabledBtn"}
+          activeClassName={styles.paginationActive}
+        />
+      </div>
       <div className="products-container">
-        {products?.map((product) => (
+        {/* OVOOOO */}
+        {/* {products?.map((product) => (
           <Product key={product._id} product={product} />
-        ))}
+        ))} */}
+        {displayUsers}
+      </div>
+      <div className={styles["pagination-container"]}>
+        <ReactPaginate
+          previousLabel={"Prethodna"}
+          nextLabel={"Sljedeća"}
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={styles.paginationBtns}
+          previousLinkClassName={"previousBtn"}
+          nextLinkClassName={"nextBtn"}
+          disabledClassName={"paginationDisabledBtn"}
+          activeClassName={styles.paginationActive}
+        />
       </div>
       {/* <FooterBanner footerBanner={bannerData && bannerData[0]} /> */}
     </>
